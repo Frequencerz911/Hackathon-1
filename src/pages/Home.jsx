b21028c5ce47f28f2ecb878700fc26770d99fb3c
Jean Louis DUMAS <dumasjl911@gmail.com>
2023-11-23 23:01:10 +0100

    Add-Music-Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,31 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const [isMusicPlaying, setIsMusicPlaying] = useState(false);
+
+  const toggleMusic = () => {
+    setIsMusicPlaying(!isMusicPlaying);
+
+    const audioElement = document.getElementById("background-music");
+    if (isMusicPlaying) {
+      audioElement.pause();
+    } else {
+      audioElement.play();
+    }
+  };
   return (
     <section className="ctn">
-        <header>
-            <img className="soundon" src="src\assets\Image\du-son.png" alt="sound on"></img>
-        </header>
+      <header>
+        <button type="button" onClick={toggleMusic} className="music-toggle">
+          {isMusicPlaying ? "🔕" : "🎵"}
+        </button>
+        <audio id="background-music" loop>
+          <source src="src\assets\Song\Kosmorider.mp3" type="audio/mpeg" />
+          <track kind="captions" label="French" srcLang="fr" src="" default />
+        </audio>
+        {/* <img className="soundon" src="src\assets\Image\du-son.png" alt="sound on"></img> */}
+      </header>
       <div className="Home">
         <h1> Spooky Bounce</h1>
         <div>
