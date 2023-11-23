a7d652390d94d32ab806eb964e50fee03612b428
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2023-11-23 20:10:07 +0100

    Fix css Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,21 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="Home">
-      <h1> Spooky Bounce</h1>
-      <div>
-      <Link to="/App">
-        <button className="buttonplay" type="button">Play the game if you dare </button>
-      </Link>
+    <section className="ctn">
+        <header>
+            <img className="soundon" src="src\assets\Image\du-son.png" alt="sound on"></img>
+        </header>
+      <div className="Home">
+        <h1> Spooky Bounce</h1>
+        <div>
+          <Link to="/App">
+            <button className="buttonplay" type="button">
+              Play the game if you dare{" "}
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
