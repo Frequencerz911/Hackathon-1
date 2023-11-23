be0b90e25b3422b274916cfd48f612e5a00fcfb1
Jean Louis DUMAS <dumasjl911@gmail.com>
2023-11-23 23:09:02 +0100

    Add-Music-App

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,8 +1,28 @@
+import React, { useState } from "react";
+
 function App() {
+  const [isMusicPlaying, setIsMusicPlaying] = useState(false);
+
+  const toggleMusic = () => {
+    setIsMusicPlaying(!isMusicPlaying);
+
+    const audioElement = document.getElementById("background-music");
+    if (isMusicPlaying) {
+      audioElement.pause();
+    } else {
+      audioElement.play();
+    }
+  };
   return (
     <section className="bgapp">
       <header>
-	  	<img className="soundon" src="src\assets\Image\du-son.png" alt="sound on"></img>
+        <button type="button" onClick={toggleMusic} className="music-toggle">
+          {isMusicPlaying ? "🔕" : "🎵"}
+        </button>
+        <audio id="background-music" loop>
+          <source src="src\assets\Song\TheShiningTheme.mp3" type="audio/mpeg" />
+          <track kind="captions" label="French" srcLang="fr" src="" default />
+        </audio>
       </header>
     </section>
   );
